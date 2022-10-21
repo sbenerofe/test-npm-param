4466b927c98ccb387aa030cecbb1a5b1aec47bee
Steven Benerofe <sbenerofe@gmail.com>
2022-10-20 20:18:20 -0700

    pass param through npm command

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var process = require('process');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
+const port = 3000;
+
+console.log('test = ' + process.env.npm_config_test);
+
+console.log('test = ' + process.env.TEST1);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+module.exports = app;
